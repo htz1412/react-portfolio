4bb055bb8a8e63207497204640bba28551e6cfb7
Harsh Gohel <30922794+htz1412@users.noreply.github.com>
2023-08-04 20:59:03 +0530

    Animate components (#1)
    * added animation to home section.
    
    * code refactored and added animation to existing components.
    
    * added animated About section.

--- src/features/components/Contact.jsx
+++ src/features/components/Contact.jsx
@@ -1,7 +1,13 @@
+import { Fade } from "react-awesome-reveal";
+
+import { SECTION_ANIMATION_DURATION } from "../constants/constants";
+
 const Contact = () => {
   return (
     <section id="contact" className="section demo-section">
-      <h1>Contact</h1>
+      <Fade direction="down" duration={SECTION_ANIMATION_DURATION}>
+        <h1>Contact</h1>
+      </Fade>
     </section>
   );
 };
--- src/features/components/NavBar.jsx
+++ src/features/components/NavBar.jsx
@@ -1,28 +1,40 @@
 import { Link } from "react-scroll";
-import "../styles/navbar.css";
+import { Fade } from "react-awesome-reveal";
+import { getNavLinks, getScrollOptions } from "../services/service";
 
 const NavBar = () => {
-  const navLinks = ["Home", "About", "Skills", "Contact"];
+  const navLinks = getNavLinks();
+  const scrollOption = getScrollOptions();
 
   return (
     <nav className="navbar container">
-      <Link to="home" spy={true} smooth={true} duration={500}>
-        Harsh
-      </Link>
+      <Fade triggerOnce direction="down" duration={800}>
+        <Link
+          className="nav-logo"
+          to="home"
+          spy={true}
+          smooth={true}
+          duration={scrollOption.duration}
+        >
+          Harsh
+        </Link>
+      </Fade>
       <ul className="nav-links">
-        {navLinks.map((navLink) => (
-          <li key={navLink} className="nav-link">
-            <Link
-              to={navLink.toLowerCase()}
-              spy={true}
-              offset={-1}
-              smooth={true}
-              duration={500}
-            >
-              {navLink}
-            </Link>
-          </li>
-        ))}
+        <Fade direction="down" duration={800} triggerOnce>
+          {navLinks.map((navLink) => (
+            <li key={navLink} className="nav-link">
+              <Link
+                to={navLink.toLowerCase()}
+                spy={true}
+                offset={scrollOption.offset}
+                smooth={true}
+                duration={scrollOption.duration}
+              >
+                {navLink}
+              </Link>
+            </li>
+          ))}
+        </Fade>
       </ul>
     </nav>
   );
